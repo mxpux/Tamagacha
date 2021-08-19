113bf2f01997aad7b65263d21fcf91ff13961574
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-18 17:20:29 -0700

    add tamagachi-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes')
+const userRoutes = require('./user-routes');
+const tamaRoutes = require('./tamagachi');
 
-router.use('/user', userRoutes)
-
+router.use('/user', userRoutes);
+router.use('/tama', tamaRoutes);
 
 module.exports = router
