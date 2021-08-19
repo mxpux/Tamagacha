fe5d76d43ffc134d7e0866f0449a633349b554c8
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-18 17:39:04 -0700

    add routes to usertama-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const tamaRoutes = require('./tamagachi');
+const userTamaRoutes = require('./usertama-routes');
 
 router.use('/user', userRoutes);
 router.use('/tama', tamaRoutes);
+router.user('/usertama', userTamaRoutes);
 
 module.exports = router
