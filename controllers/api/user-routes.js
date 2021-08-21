1e34116fa2a5063fa9ccccb364015cea2b6dfc0a
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-20 17:51:22 -0700

    user signup post route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     // const dbUserData = await User.findByPk(req.params.id);
-    // const userData = dbUserData.map((user) => user.get({ plain : true}))
+    
     // res.status(200).json(dbUserData);
     res.status(200).json({message: "test passed"})
 
@@ -33,6 +33,25 @@ router.get('/:id', async (req, res) => {
 //CREATE NEW USER
 // SIGNUP
 //TODO: POST ROUTE
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.username = req.body.username;
+
+      res.status(200).json(dbUserData);
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
 
 //LOGIN
 //TODO: POST ROUTE
