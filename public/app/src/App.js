30c35d26b29d9280eb378b5784bb656079c5cc41
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-23 02:17:47 -0700

    start login form

--- public/app/src/App.js
+++ public/app/src/App.js
@@ -2,11 +2,39 @@ import React from 'react';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Homepage from './components/Homepage';
+import Hamburger from 'hamburger-react';
+import './components/style.css';
+import Login from './components/login'
 
 function App() {
   return (
     <div>
-      <Home />
+      <div class="header">
+        <h1>TAMAGACHA</h1>
+        <div class='menu'>
+          <Hamburger distance="md" color="lightyellow" onToggle={toggled => {
+            if (toggled) {
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Login</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">My Tama</a>
+                </li>
+              </ul>
+
+            } else {
+              // close a menu
+            }
+          }} />
+        </div>
+      </div>
       <Nav />
       <Homepage />
     </div>
---
+++ public/app/src/components/login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './login.css';
+
+function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    return name === 'firstName' ? setFirstName(value) : setLastName(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    alert(`Hello ${firstName} ${lastName}`);
+    setFirstName('');
+    setLastName('');
+  };
+
+  return (
+    <div>
+      <p>
+        Hello {firstName} {lastName}
+      </p>
+      <form className="form">
+        <input
+          value={firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          value={lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
