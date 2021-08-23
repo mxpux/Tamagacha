30e7c083350868d4d551a5e35b3dbcb4b3f00172
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-22 17:14:25 -0700

    create nav bar in react

---
+++ public/app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './components/Home';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ public/app/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h2>Tamagacha</h2>
+    </div>
+  );
+}
+
+export default Home;
