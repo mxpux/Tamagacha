a580e0977af3ac78e061bfc37b4f927507925da9
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-28 15:46:03 -0700

    styling egg

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,17 @@ import Login from './components/login';
 import DropDown from './components/MenuDrop';
 import Ttt from './components/Ttt';
 import Profile from './components/Profile'
+import Gacha from './components/gacha_page/gacha'
 
-
-import Matching from './components/Matching';
+// import Matching from './components/Matching';
 
 function App() {
   return (
     <div>
       <Home />
       <Homepage />
-      <Matching />
+      {/* <Matching /> */}
+      <Gacha />
     </div>
   );
 }
