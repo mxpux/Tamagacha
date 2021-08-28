934cec84db4a965dfaff607494103b904377ba64
MPx <hellomariap@gmail.com>
2021-08-28 15:11:27 -0700

    Added winning combos

--- client/src/components/Ttt.js
+++ client/src/components/Ttt.js
@@ -1,5 +1,24 @@
 import React, { useState } from 'react';
 import './ttt.css';
+import x from '../styles/test1.png';
+
+	const winningComb = (squares) => {
+		var combos = {
+			across: [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+			],
+			down: [
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+			],
+			diagonal: [
+				[0, 4, 8],
+				[2, 4, 6],
+			],
+		};
 
 		<div className='container'>
 		
