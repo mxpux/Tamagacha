2afe46c135a27f765fce55310001177e8fbd22e0
MPx <hellomariap@gmail.com>
2021-08-31 12:02:40 -0700

    styling for minigame page

--- client/src/components/Minigame/Minigamepage.js
+++ client/src/components/Minigame/Minigamepage.js
@@ -4,26 +4,28 @@ import TamaTTT from '../../assets/TamaTTT.png';
 import './minigamepage.css';
 
 function MinigamePage() {
-    
+
     return (
-        <div>
-            <h2>Which Minigame would you like to play?</h2>
-            <div className="minigame-container">
-                <div className="minigame-card">
+        <div className="minigame-container">
+            {/* <div className="minigame-container"> */}
+                <div className="minigame-cardcontainer">
+                     <h2 class="minigame-title">Which Minigame would you like to play?</h2>
+                    <div className="minigame-card">
                     <h3>Matching Game</h3>
-                    <img 
-                    className="minigame-card-img"
-                    src={TamaMatch}
-                    alt="Tama Match" />
-                </div>
-                <div className="minigame-card">
+                    <img
+                        className="minigame-card-img"
+                        src={TamaMatch}
+                        alt="Tama Match" />
+                    </div>
+                    <div className="minigame-card">
                     <h3>Tic Tac Toe</h3>
-                    <img 
-                    className="minigame-card-img"
-                    src={TamaTTT}
-                    alt="Tama Tic Tac Toe" />
+                    <img
+                        className="minigame-card-img"
+                        src={TamaTTT}
+                        alt="Tama Tic Tac Toe" />
+                    </div>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
