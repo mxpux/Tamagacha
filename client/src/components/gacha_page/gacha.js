372532b49959faec8f5c1c985c16389c7096723c
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-02 13:43:40 -0700

    add howlerjs pop sound on gacha page

--- client/src/components/gacha_page/gacha.js
+++ client/src/components/gacha_page/gacha.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import './gacha.css'
+import React, { Component } from 'react';
+import {Howl, Howler} from 'howler';
+import useSound from 'use-sound';
+import pop from './pop.mp3'
+import './gacha.css';
 
-function Gacha () {
-  return(
+const sound = new Howl ({
+  src: [pop],
+  loop: true,
+  volume: 0.1,
+});
+
+function Gacha() {
+  return (
     <>
       <div id='egg-container'>
         <p className='gacha-title'>What will hatch!?</p>
-        <egg>
-          
+        
+        <egg onMouseOver={sound.play()}>
         </egg>
       </div>
     </>
   )
 }
+
 
 export default Gacha
