fc42f8d284d8c5f562bbed9c07778193ad907464
Eajay Delos Santos <edelos1524@hotmail.com>
2021-09-03 13:06:58 -0700

    reseeded tama's with names

--- server/seeds/tamaData.js
+++ server/seeds/tamaData.js
@@ -2,27 +2,27 @@ const { Tama } = require('../models')
 
 const tamaData = [
   {
-    name: 'tama1',
+    name: 'Mametchi',
     description: 'this is tama1 description',
     pictures: 'https://i.imgur.com/fIqgUBA.png'
   },
   {
-    name: 'tama2',
+    name: 'Kuchipatchi',
     description: 'this is tama2 description',
     pictures: 'https://i.imgur.com/9wKRnPZ.png'
   },
   {
-    name: 'tama3',
+    name: 'Violetchi',
     description: 'this is tama3 description',
     pictures: 'https://i.imgur.com/KOqDtIj.png'
   },
   {
-    name: 'tama4',
+    name: 'Orenchi',
     description: 'this is tama4 description',
     pictures: 'https://i.imgur.com/NxR6vjy.png'
   },
   {
-    name: 'tama5',
+    name: 'Gozarutchi',
     description: 'this is tama5 description',
     pictures: 'https://i.imgur.com/ZaGPPo9.png'
   }
