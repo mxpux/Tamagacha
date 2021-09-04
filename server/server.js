b14cfb8a5ef6d55a859be0936d947ad14f8e1795
Eajay Delos Santos <edelos1524@hotmail.com>
2021-09-04 11:32:14 -0700

    heroku testing build

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+// app.use(routes); test
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
