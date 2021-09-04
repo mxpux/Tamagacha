d34f195bf3dc3614b3461016f95ced1add87719d
Eajay Delos Santos <edelos1524@hotmail.com>
2021-09-04 13:13:36 -0700

    heroku testing build path reqorked

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(routes); test
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
 sequelize.sync({ force: false }).then(() => {
